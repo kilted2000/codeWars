3b3a26356ad7270c92eb0589786102c3383b7dfa
Richard <richardlwhittington@gmail.com>
2023-11-23 00:01:17 -0600

    add solution

---
+++ 8kyu/dataObj.js
@@ -0,0 +1,15 @@
+// Task
+// Given a function animal, accept 1 parameter:obj like this:
+
+// {name:"dog",legs:4,color:"white"}
+// and return a string like this:
+
+// "This white dog has 4 legs."
+
+// my solution
+
+function animal(obj){
+    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+  }
+  
+
