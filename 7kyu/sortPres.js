2aa3ad88cbe26626fb2c7a2a722d29934b272ecd
Richard <richardlwhittington@gmail.com>
2023-12-27 00:02:51 -0800

    add solution

---
+++ 7kyu/sortPres.js
@@ -0,0 +1,16 @@
+// Santa asked his organizer to order the characters in alphabetical order, but the Elf fell asleep from consuming too much hot chocolate and candy canes! Can you help him out?
+
+// Sort the Gift Code
+// Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+
+// Examples (Input -- => Output):
+// "abcdef"                      -- => "abcdef"
+// "pqksuvy"                     -- => "kpqsuvy"
+// "zyxwvutsrqponmlkjihgfedcba"  -- => "abcdefghijklmnopqrstuvwxyz"
+
+// my solution
+
+
+function sortGiftCode(code){
+    return code.split("").sort().join("");
+  }
