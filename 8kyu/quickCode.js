2779de02e0d48127407291169ee80252e2132908
Richard <richardlwhittington@gmail.com>
2024-01-14 00:07:33 -0600

    add solution

--- 8kyu/quickCode.js
+++
@@ -1,8 +0,0 @@
-//Code as fast as you can! You need to double the integer and return it.
-
-//my Solution
-
-function doubleInteger(i) {
-    let double = i * 2
-    return double;
-  }
--- 8kyu/removeEx.js
+++
@@ -1,7 +0,0 @@
-// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
-
-// my solution
-
-function removeExclamationMarks(s) {
-    return s.replace(/!/g, '');
-  }
