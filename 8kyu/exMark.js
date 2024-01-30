76ec34b338833f74b4b5689023efea5a94aea39d
Richard <richardlwhittington@gmail.com>
2024-01-30 10:04:27 -0600

    add solution

---
+++ 8kyu/exMark.js
@@ -0,0 +1,16 @@
+// Description:
+// Remove all exclamation marks from the end of sentence.
+
+// Examples
+// "Hi!"     ---> "Hi"
+// "Hi!!!"   ---> "Hi"
+// "!Hi"     ---> "!Hi"
+// "!Hi!"    ---> "!Hi"
+// "Hi! Hi!" ---> "Hi! Hi"
+// "Hi"      ---> "Hi"
+
+// my solution
+
+function remove(s) {
+    return s.replace(/!+$/, '')
+}
