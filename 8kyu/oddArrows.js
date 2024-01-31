5e1ac4f293859c5ceb6525fb320f559aa16882f1
Richard <richardlwhittington@gmail.com>
2024-01-31 00:31:08 -0600

    add solution

---
+++ 8kyu/oddArrows.js
@@ -0,0 +1,10 @@
+// Time to test your basic knowledge in functions! Return the odds from a list:
+
+// [1, 2, 3, 4, 5]  -->  [1, 3, 5]
+// [2, 4, 6]        -->  []
+
+// my solution
+
+function odds(values){
+    return values.filter(value => value % 2 !== 0)
+}
